4925218ef72156f0b7550862cd16eb5e495064fa
MD ¥ <92609462+mourao61@users.noreply.github.com>
2021-11-02 12:32:19 -0300

    Add files via upload

---
+++ revendamais/simulacao.js
@@ -0,0 +1,23 @@
+var frmmodelo;
+var frmvalor = parseInt();
+var divMostrar;
+
+
+// Botao cadastrar add evento
+
+btnSimular.addEventListener('click', function(){
+   frmmodelo = modelo.value
+   frmvalor = valor.value
+   divMostrar = document.getElementById('mostrarSimulacao')
+   alert('Simulação realizada com sucesso!')
+    var frmentrada = frmvalor / 2
+    var frmfinal = frmvalor / 2 / 60 * 0.70 + frmvalor / 2 / 60 .toFixed(2) 
+    
+
+divMostrar.innerHTML = `
+<div>Modelo do veiculo :  <strong>${frmmodelo}</strong></div>
+<div>Valor do veiculo :  <strong>${frmvalor}</strong></div>
+<div>Valor da entrada :  <strong>${frmentrada}</strong></div>
+<div>60x de R$ :  <strong>${frmfinal}</strong></div>
+`
+} ) 
