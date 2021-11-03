ad8ed56a6cc3187564c27ed42ce82996d36042e0
MD ¥ <92609462+mourao61@users.noreply.github.com>
2021-11-03 09:21:46 -0300

    Update simulacao.js

--- simulacao.js
+++ simulacao.js
@@ -11,13 +11,13 @@ btnSimular.addEventListener('click', function(){
    divMostrar = document.getElementById('mostrarSimulacao')
    alert('Simulação realizada com sucesso!')
     var frmentrada = frmvalor / 2
-    var frmfinal = frmvalor / 2 / 60 * 0.70 + frmvalor / 2 / 60.toFixed(2)
+    var frmfinal = frmvalor / 2 / 60 * 0.70 + frmvalor / 2 / 60
     
 
 divMostrar.innerHTML = `
 <div>Modelo do veiculo :  <strong>${frmmodelo}</strong></div>
 <div>Valor do veiculo :  <strong>${frmvalor}</strong></div>
 <div>Valor da entrada :  <strong>${frmentrada}</strong></div>
-<div>60x de R$ :  <strong>${frmfinal}</strong></div>
+<div>60x de R$ :  <strong>${frmfinal}.toFixed(2)</strong></div>
 `
 } ) 
