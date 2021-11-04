20ae83b53d6385a399e855d02e15f74ee0216f01
MD ¥ <92609462+mourao61@users.noreply.github.com>
2021-11-04 10:56:07 -0300

    foi adicionado a seleção de parcelas

--- simulacao.js
+++ simulacao.js
@@ -5,17 +5,21 @@ var divMostrar;
 btnSimular.addEventListener('click', function(){
    frmmodelo = modelo.value
    frmvalor = valor.value
+   frmPareclas = parseInt(qtdParcelas.value)
+   
    divMostrar = document.getElementById('mostrarSimulacao')
    alert('Simulação realizada com sucesso!')
-    var frmentrada = frmvalor / 2
-    var frmfinal = frmvalor / 2 / 60 * 0.70 + frmvalor / 2 / 60
+    
+   var frmentrada = ((frmvalor/2)/frmPareclas)
+   var frmfinal = frmentrada*1.70
+   var frmvalor2 =  frmvalor/2
     
 // .toFixed(2)
 
 divMostrar.innerHTML = `
 <div>Modelo do veículo :  <strong>${frmmodelo}</strong></div>
 <div>Valor do veículo :  <strong>R$</strong> <strong>${frmvalor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
-<div>Valor da entrada :  <strong>${frmentrada.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
-<div>60x no valor de :  <strong>${frmfinal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
+<div>Valor da entrada :  <strong>${frmvalor2.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
+<div><strong>${frmPareclas}</strong> Parcelas no valor de : <strong>${frmfinal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
 `
 } ) 
