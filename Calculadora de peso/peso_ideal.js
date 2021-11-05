0605a3f738f507cd6a026b87e11a71a193ca652e
MD ¥ <92609462+mourao61@users.noreply.github.com>
2021-11-05 08:49:47 -0300

    Add files via upload

--- Calculadora de peso/peso_ideal.js
+++ Calculadora de peso/peso_ideal.js
@@ -1,26 +1,21 @@
-var frmnome;
-var frmpeso = parseInt();
-var frmaltura = parseInt();
-var divMostrar;
-
-btnCalcular.addEventListener('click', function(){
-   frmnome = nome.value
-   frmpeso = peso.value
-   frmaltura = altura.value
-   frmsexo = parseInt(sexoMasculino.value)
-   frmsexo = parseInt(sexoFeminino.value)
-   
-   divpesoIdeal = document.getElementById('mostrarSimulacao')
-       
-   var frmfinal = frmentrada*1.70
-   var frmvalor2 =  frmvalor/2
+btnCalcular.addEventListener('click', function () { 
     
-// .toFixed(2)
+    Altura = altura.value
+    sexoMasculino
 
-divpesoIdeal.innerHTML = `
-<div>Modelo do veículo :  <strong>${frmmodelo}</strong></div>
-<div>Valor do veículo :  <strong>R$</strong> <strong>${frmvalor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
-<div>Valor da entrada :  <strong>${frmvalor2.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
-<div><strong>${frmPareclas}</strong> Parcelas no valor de : <strong>${frmfinal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></div>
-`
-} ) 
+    
+    if(sexoMasculino.checked){
+       fatorPeso = 22
+    } else {
+       fatorPeso = 21
+    }
+
+    peso = fatorPeso * Math.pow(Altura, 2).toFixed(2)
+
+    
+    Peso = document.getElementById('pesoIdeal')
+    Peso.innerHTML = `
+       
+    <p>Seu peso ideal é ${peso} Kg.</p>`
+})
+
