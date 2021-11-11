d35b1046bf9bfc51c2a3de30c8c9669a321c9ca8
MD ¥ <92609462+mourao61@users.noreply.github.com>
2021-11-11 20:56:23 -0300

    Add files via upload

--- repeticao2.js
+++ repeticao2.js
@@ -6,10 +6,12 @@ btnRegistrar.addEventListener("click", repeticao)
 function repeticao() {
     var frmNome = nome.value;   
     var frmQuantidade = Number(quantidade.value);
-        
-    resposta += `<br> Ferramenta: ${frmNome} <br>`
-    resposta += `Numero de vezes: ${frmQuantidade} <br>`
     
-       
+    resposta += `<br> Ferramenta: ${frmNome} <br>` // exibe o nome da ferramenta 
+    resposta += `Numero de vezes: ${frmQuantidade} <br>` // conta o numero de repetições
+    
+    for (let ferramenta = ''; ferramenta <= frmQuantidade; ferramenta++) {  //Repete o nome da ferramenta dividido por |
+        resposta += `|${frmNome}`;
+    }  
     listaFerramentas.innerHTML = resposta 
 }
